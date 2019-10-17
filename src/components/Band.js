6dfba4091e0a42376c783008904788f377d990ed
Kane Koo <kanekoo@gmail.com>
2019-10-17 10:38:03 -0400

    Done.

--- src/components/Band.js
+++ src/components/Band.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
+import BandInput from '../components/BandInput'
 
 class Band extends Component {
 
   render() {
+    console.log(this.props)
     return(
       <div>
-        Band Component
+        {<li id={this.props.band.id}>{this.props.band.bandName} <button onClick={()=>this.props.delete(this.props.band.id)}</li>}
       </div>
     );
   }
--- src/components/BandInput.js
+++ src/components/BandInput.js
@@ -1,21 +1,25 @@
 import React, { Component } from 'react';
+import uuid from 'uuid';
 
 class BandInput extends Component {
 
   state = {
+    id:0,
     bandName: ''
   }
 
   handleOnChange(event) {
     this.setState({
+      id: uuid(),
       bandName: event.target.value,
     });
   }
 
   handleOnSubmit(event) {
     event.preventDefault();
-    this.props.addBand(this.state.bandName);
+    this.props.addBand(this.state);
     this.setState({
+      id:0,
       bandName: '',
     });
   }
